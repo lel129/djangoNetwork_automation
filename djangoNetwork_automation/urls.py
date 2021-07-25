91c9843381d1bc397181940c9eaefc7aad96f0b2
lel129 <77856890+lel129@users.noreply.github.com>
2021-07-26 01:56:23 +0700

    Commit

--- djangoNetwork_automation/urls.py
+++ djangoNetwork_automation/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 import mimetypes
+
+from django.views.static import serve
+
 mimetypes.add_type("text/css", ".css", True)
 
 urlpatterns = [
@@ -16,9 +19,14 @@ urlpatterns = [
     url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
     path('admin/', admin.site.urls),
     path('',include('Netwok_automation.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+
+
 
 
 

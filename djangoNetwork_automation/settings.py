bc0aa0afab35248b7a7ff7fdba1bdbae3797ae34
lel129 <77856890+lel129@users.noreply.github.com>
2021-07-26 01:27:27 +0700

    Commit

--- djangoNetwork_automation/settings.py
+++ djangoNetwork_automation/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f0rrsy1@@z4p&(e^&)yzdq86#d0@vw2&q0^*az+kmm#t(e(-nz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = [ '127.0.0.1','networkautomationdjango.herokuapp.com/']
 
 
 # Application definition
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
